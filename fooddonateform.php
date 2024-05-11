4a14aafef4bef8c0da2dc8a8c954ef9c5f829f67
Divyanshu Ranjan <146622948+divyansh9521@users.noreply.github.com>
2024-05-11 18:00:31 +0530

    Create fooddonateform.php

---
+++ fooddonateform.php
@@ -0,0 +1,167 @@
+<?php
+include("login.php"); 
+if($_SESSION['name']==''){
+	header("location: signin.php");
+}
+// include("login.php"); 
+$emailid= $_SESSION['email'];
+$connection=mysqli_connect("localhost","root","");
+$db=mysqli_select_db($connection,'demo');
+if(isset($_POST['submit']))
+{
+    $foodname=mysqli_real_escape_string($connection, $_POST['foodname']);
+    $meal=mysqli_real_escape_string($connection, $_POST['meal']);
+    $category=$_POST['image-choice'];
+    $quantity=mysqli_real_escape_string($connection, $_POST['quantity']);
+    $phoneno=mysqli_real_escape_string($connection, $_POST['phoneno']);
+    $district=mysqli_real_escape_string($connection, $_POST['district']);
+    $address=mysqli_real_escape_string($connection, $_POST['address']);
+    $name=mysqli_real_escape_string($connection, $_POST['name']);
+  
+
+ 
+
+
+
+    $query="insert into food_donations(email,food,type,category,phoneno,location,address,name,quantity) values('$emailid','$foodname','$meal','$category','$phoneno','$district','$address','$name','$quantity')";
+    $query_run= mysqli_query($connection, $query);
+    if($query_run)
+    {
+
+        echo '<script type="text/javascript">alert("data saved")</script>';
+        header("location:delivery.html");
+    }
+    else{
+        echo '<script type="text/javascript">alert("data not saved")</script>';
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Donate</title>
+    <link rel="stylesheet" href="loginstyle.css">
+</head>
+<body style="    background-color: #06C167;">
+    <div class="container">
+        <div class="regformf" >
+    <form action="" method="post">
+        <p class="logo">Food <b style="color: #06C167; ">Donate</b></p>
+        
+       <div class="input">
+        <label for="foodname"  > Food Name:</label>
+        <input type="text" id="foodname" name="foodname" required/>
+        </div>
+      
+      
+        <div class="radio">
+        <label for="meal" >Meal type :</label> 
+        <br><br>
+
+        <input type="radio" name="meal" id="veg" value="veg" required/>
+        <label for="veg" style="padding-right: 40px;">Veg</label>
+        <input type="radio" name="meal" id="Non-veg" value="Non-veg" >
+        <label for="Non-veg">Non-veg</label>
+    
+        </div>
+        <br>
+        <div class="input">
+        <label for="food">Select the Category:</label>
+        <br><br>
+        <div class="image-radio-group">
+            <input type="radio" id="raw-food" name="image-choice" value="raw-food">
+            <label for="raw-food">
+              <img src="img/raw-food.png" alt="raw-food" >
+            </label>
+            <input type="radio" id="cooked-food" name="image-choice" value="cooked-food"checked>
+            <label for="cooked-food">
+              <img src="img/cooked-food.png" alt="cooked-food" >
+            </label>
+            <input type="radio" id="packed-food" name="image-choice" value="packed-food">
+            <label for="packed-food">
+              <img src="img/packed-food.png" alt="packed-food" >
+            </label>
+          </div>
+          <br>
+        </div>
+        <div class="input">
+        <label for="quantity">Quantity:(number of person /kg)</label>
+        <input type="text" id="quantity" name="quantity" required/>
+        </div>
+       <b><p style="text-align: center;">Contact Details</p></b>
+        <div class="input">
+          
+      <div>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name"value="<?php echo"". $_SESSION['name'] ;?>" required/>
+      </div>
+      <div>
+        <label for="phoneno" >PhoneNo:</label>
+      <input type="text" id="phoneno" name="phoneno" maxlength="10" pattern="[0-9]{10}" required />
+        
+      </div>
+      </div>
+        <div class="input">
+        <label for="location"></label>
+        <label for="district" style="position:relative; top: 22px;">State:</label>
+<select id="district" name="district" style="padding:10px; position:relative; top:20px; left:2px;">
+<option value="Select State">Select State</option>
+<option value="Andhra Pradesh">Andhra Pradesh</option>
+<option value="Arunachal Pradesh">Arunachal Pradesh</option>
+<option value="Assam">Assam</option>
+<option value=" Bihar"> Bihar</option>
+<option value="Chhattisgarh">Chhattisgarh</option>
+<option value="Goa">Goa</option>
+<option value="Gujarat">Gujarat</option>
+<option value="Haryana">Haryana</option>
+<option value="Himachal Pradesh">Himachal Pradesh</option>
+<option value="Jharkhand">Jharkhand</option>
+<option value="Karnataka">Karnataka</option>
+<option value="Kerala">Kerala</option>
+<option value="Maharashtra">Maharashtra</option>
+<option value="Madhya Pradesh">Madhya Pradesh</option>
+<option value="Manipur">Manipur</option>
+<option value="Meghalaya">Meghalaya</option>
+<option value="Mizoram">Mizoram</option>
+<option value="Nagaland">Nagaland</option>
+<option value="Odisha">Odisha</option>
+<option value="Punjab">Punjab</option>
+<option value="Rajasthan">Rajasthan</option>
+<option value="Sikkim">Sikkim</option>
+<option value="Tamil Nadu">Tamil Nadu</option>
+<option value="Tripura">Tripura</option>
+<option value="Telangana">Telangana</option>
+<option value="Uttar Pradesh">Uttar Pradesh</option>
+<option value="Uttarakhand">Uttarakhand</option>
+<option value="West Bengal">West Bengal</option>
+</select> 
+
+<p>Address:</p>
+<input type="text" id="address" name="address" required/><br>
+<style>
+  p{
+    width: 100%;
+    position: relative;
+    top: 19px;
+  }
+</style>
+      
+       
+       
+        </div>
+        <div class="btn">
+            <button type="submit" name="submit"> submit</button>
+     
+        </div>
+     </form>
+     </div>
+   </div>
+     
+    
+</body>
+</html>
